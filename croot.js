890941754ea4fa344249ab56c0460e453e0859c0
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-14 05:50:45 +0700

    getrandomcolorname

--- croot.js
+++ croot.js
@@ -5,4 +5,32 @@ export function getRandomColor() {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
+
+export function getRandomColorName() {
+  let colors = [
+    "yellow",
+    "violet",
+    "green",
+    "red",
+    "rose",
+    "pink",
+    "fuchsia",
+    "purple",
+    "indigo",
+    "blue",
+    "sky",
+    "cyan",
+    "teal",
+    "emerald",
+    "lime",
+    "amber",
+    "orange",
+    "stone",
+    "neutral",
+    "zinc",
+    "gray",
+    "slate"
+  ]
+  return colors[Math.floor(Math.random() * colors.length)];
+}
